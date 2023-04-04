e8236151e1b1f76bc5f07888112c76957e53dc2f
faizanUnico <faizanzaid789@gmail.com>
2023-04-04 18:02:20 +0530

    created api to leave channel

---
+++ server/src/v1/controllers/channels/leaveChannel.js
@@ -0,0 +1,32 @@
+const Channel = require("../../models/Channel");
+const User = require("../../models/User");
+
+const leaveChannel = async (req, res) => {
+  if (!req.params.channelId) {
+    return res.status(400).json({ message: "Channel Id is required" });
+  }
+  if (!req.body.userId) {
+    return res.status(400).json({ message: "User id is required" });
+  }
+
+  try {
+    const user = await User.findById({ _id: req.body.userId });
+    if (!user) {
+      return res.status(403).json({ message: "You are not authorized" });
+    }
+
+    const channel = await Channel.findById({ _id: req.params.channelId });
+    const users = JSON.parse(JSON.stringify(channel.users));
+    const removeUser = users.filter((user) => user._id !== req.body.userId);
+
+    console.log(removeUser);
+    channel.users = removeUser;
+    await channel.save();
+
+    return res.status(200).json({ success: true });
+  } catch (err) {
+    return res.status(500).json({ message: err.message });
+  }
+};
+
+module.exports = leaveChannel;
--- server/src/v1/routes/channelRoutes.js
+++ server/src/v1/routes/channelRoutes.js
@@ -4,13 +4,15 @@ const createChannel = require("../controllers/channels/createChannel");
 const authenticateUser = require("../middleware");
 const joinChannel = require("../controllers/channels/joinChannel");
 const getSingleChannel = require("../controllers/channels/getSingleChannel");
+const leaveChannel = require("../controllers/channels/leaveChannel");
 const router = express.Router();
 
-// get all channels
+// all channels
 
 router.get("/channel", authenticateUser, getChannels);
 router.post("/channel", authenticateUser, createChannel);
 router.post("/channel/:channelId", authenticateUser, joinChannel);
 router.get("/getChannel/:channelId", authenticateUser, getSingleChannel);
+router.post("/leaveChannel/:channelId", authenticateUser, leaveChannel)
 
 module.exports = router;
