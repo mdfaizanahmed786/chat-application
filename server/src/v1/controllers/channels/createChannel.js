661c7d46508f900b6b68a1044460560eb24e18db
faizanUnico <faizanzaid789@gmail.com>
2023-04-08 22:54:57 +0530

    some changes were made to leave channel api route

--- server/src/v1/controllers/channels/createChannel.js
+++ server/src/v1/controllers/channels/createChannel.js
@@ -7,14 +7,13 @@ const createChannel = async (req, res) => {
       message: "Please provide a name and description for the channel",
     });
 
-    const channelName = req.body.name;
-    const findChannel=await Channel.findOne({name:channelName})
-    if(findChannel){
-      return res.status(400).json({
-        message: "Channel already exists",
-      });
-    }
-    
+  const channelName = req.body.name;
+  const findChannel = await Channel.findOne({ name: channelName });
+  if (findChannel) {
+    return res.status(400).json({
+      message: "Channel already exists",
+    });
+  }
 
   const channel = new Channel({
     name: req.body.name,
@@ -22,15 +21,26 @@ const createChannel = async (req, res) => {
     createdBy: req.user._id,
   });
   try {
-
-
     const savedChannel = await channel.save();
 
-    const user=await User.findById({_id:req.user._id})
+    const user = await User.findById({ _id: req.user._id });
     const userObj = JSON.parse(JSON.stringify(user));
-  
-    channel.users.push({_id:userObj._id,name:userObj.name, email:userObj.email})
+
+    channel.users.push({
+      _id: userObj._id,
+      name: userObj.name,
+      email: userObj.email,
+    });
     await channel.save();
+
+    user.channels.push({
+      _id: savedChannel._id,
+      name: savedChannel.name,
+      users: savedChannel.users,
+      createdBy: savedChannel.createdBy,
+    });
+    await user.save();
+
     res.status(201).json({ success: true, channel: savedChannel });
   } catch (err) {
     res.status(500).json({ message: err.message });
--- server/src/v1/controllers/channels/leaveChannel.js
+++ server/src/v1/controllers/channels/leaveChannel.js
@@ -7,42 +7,48 @@ const leaveChannel = async (req, res) => {
   }
   if (!req.body.userId) {
     return res.status(400).json({ message: "Invalid request" });
-
   }
 
-  if(Boolean(req.body.creator)){
-    const deleteChannel=await Channel.findByIdAndDelete({_id:req.params.channelId});
-    if(!deleteChannel){
-      return res.status(400).json({message:"Channel not found"});
+  if (Boolean(req.body.creator)) {
+    const deleteChannel = await Channel.findByIdAndDelete({
+      _id: req.params.channelId,
+    });
+    const user = await User.findById({ _id: req.body.userId });
+    const filteredUsers = JSON.parse(JSON.stringify(user.channels));
+    const removeChannel = filteredUsers.filter(
+      (channel) => channel._id !== req.params.channelId
+    );
+    user.channels = removeChannel;
+    await user.save();
+    if (!deleteChannel) {
+      return res.status(400).json({ message: "Channel not found" });
     }
-    return res.status(200).json({success:true});
-  }
-  else{
-
+    return res.status(200).json({ success: true });
+  } else {
     try {
       const user = await User.findById({ _id: req.body.userId });
       if (!user) {
         return res.status(403).json({ message: "You are not authorized" });
       }
-  
+
       const channel = await Channel.findById({ _id: req.params.channelId });
       const users = JSON.parse(JSON.stringify(channel.users));
       const removeUser = users.filter((user) => user._id !== req.body.userId);
-  
-     
+
       channel.users = removeUser;
       await channel.save();
-      const filteredUsers=JSON.parse(JSON.stringify(user.channels));
-      const removeChannel= filteredUsers.filter((channel) => channel._id !== req.params.channelId);
+      const filteredUsers = JSON.parse(JSON.stringify(user.channels));
+      const removeChannel = filteredUsers.filter(
+        (channel) => channel._id !== req.params.channelId
+      );
       user.channels = removeChannel;
       await user.save();
-  
+
       return res.status(200).json({ success: true });
     } catch (err) {
       return res.status(500).json({ message: err.message });
     }
   }
-
 };
 
 module.exports = leaveChannel;
